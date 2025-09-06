50388805ba83016e5cc68671bb83f0b31512394a
ishigen425 <g4.fine.yamanashi@gmail.com>
2025-09-06 22:26:48 +0900

    fix vote bug

--- supabase/migrations/20250906065816_create_players_table.sql
+++
@@ -1,6 +0,0 @@
-create table players (
-  id uuid default gen_random_uuid() primary key,
-  name text not null,
-  profile text,
-  created_at timestamp with time zone default timezone('utc'::text, now()) not null
-);
