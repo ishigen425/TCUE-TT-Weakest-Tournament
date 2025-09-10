cc006f07a1ddc28e548f78a3cf7ff23ae9be2f33
ishigen425 <g4.fine.yamanashi@gmail.com>
2025-09-10 22:44:15 +0900

    fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import PlayerProfile from './components/PlayerProfile'
 import VoteChart from './components/VoteChart'
 import EndRollColumn from './components/EndRollColumn'
